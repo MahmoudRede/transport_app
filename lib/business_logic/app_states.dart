71323275d998e947e9d5b9109f341db44698277a
DELL <mahmoud180280@fci.bu.edu.eg>
2024-03-06 00:18:10 +0200

    auth with phone

--- lib/business_logic/app_states.dart
+++ lib/business_logic/app_states.dart
@@ -18,3 +18,31 @@ class UploadOrderImageSuccessState extends AppStates{}
 class UploadOrderImageErrorState extends AppStates{}
 
 class LunchToWhatsAppSuccessState extends AppStates{}
+
+class VerifyPhoneLoadingState extends AppStates{}
+class VerifyPhoneSuccessState extends AppStates{}
+class LoginWithPhoneErrorState extends AppStates{}
+
+class UploadUserInfoLoadingState extends AppStates{}
+class UploadUserInfoSuccessState extends AppStates{}
+class UploadUserInfoErrorState extends AppStates{}
+
+class GetUserLoadingState extends AppStates{}
+class GetUserSuccessState extends AppStates{}
+class GetUserErrorState extends AppStates{}
+
+class SignInPhoneLoadingState extends AppStates{}
+class SignInPhoneSuccessState extends AppStates{}
+class SignInPhoneErrorState extends AppStates{}
+
+class ResendCodeLoadingState extends AppStates{}
+class ResendCodeSuccessState extends AppStates{}
+class ResendCodeErrorState extends AppStates{}
+
+class ResendCodeTimerState extends AppStates{}
+class ResendCodeTimerFinishedState extends AppStates{}
+
+class CheckUserIdLoadingState extends AppStates{}
+class CheckUserIdSuccessState extends AppStates{}
+class CheckUserIdErrorState extends AppStates{}
+
